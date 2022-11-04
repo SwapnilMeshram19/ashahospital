6486f4bc2b9cf28aeb405e2b5fb10aeb002666f7
Swapnil Arun Meshram <swapnilm1908@gmail.com>
2022-11-05 01:13:00 +0530

    .

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,13 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    './node_modules/tw-elements/dist/js/**/*.js'
+    './node_modules/tw-elements/dist/js/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('tw-elements/dist/plugin')
+    require('tw-elements/dist/plugin'),
+    require('@tailwindcss/forms')
   ],
 }
