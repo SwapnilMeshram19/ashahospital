5ac585367a4b0ad68cecc9606179988de1134a2e
Swapnil Arun Meshram <swapnilm1908@gmail.com>
2022-12-02 23:46:47 +0530

    .

--- src/components/eventComponents/EventCard.tsx
+++ src/components/eventComponents/EventCard.tsx
@@ -1,30 +1,37 @@
 import React, { FC } from "react";
+import { Navigate,useNavigate} from "react-router-dom";
 import { eventI } from "../../features/events/getEvent";
 
-interface propsI {
+export interface eventPropsI {
   eventData: eventI;
 }
 
-export const EventCard: FC<propsI> = (props) => {
+export const EventCard: FC<eventPropsI> = (props) => {
   const data = props.eventData;
+  const navigate=useNavigate();
 
   return (
     <div className="rounded-lg bg-white shadow-lg">
       <div className="flex justify-center">
         <img
-          className="object-cover h-[250px] rounded-t-lg"
+          className="h-[250px] rounded-t-lg object-cover"
           src={`data:${data.images[0].contentType};base64,${data.images[0].imageBase64}`}
           alt=""
         />
       </div>
       <div className="p-4">
-        <h5 className="text-xl font-extrabold tracking-wide text-sky-900">{data.title}</h5>
-        <div className=" line-clamp-3 text-base leading-5 text-slate-600">
-     {data.description}
+        <h5 className="text-xl font-extrabold tracking-wide text-sky-900">
+          {data.title}
+        </h5>
+        <div className=" mt-4 text-base leading-5 text-slate-600 line-clamp-3">
+          {data.description}
         </div>
       </div>
-      <div className="w-full cursor-pointer bg-slate-200 p-2 text-center text-xl text-blue-600 hover:bg-slate-300">
-        show more
+      <div
+        className="my-4 ml-auto mr-6 flex w-32 cursor-pointer justify-center rounded-md bg-sky-900 py-1 text-center text-base text-white hover:bg-sky-700"
+        onClick={()=>navigate(`/event/${data._id}/${data.title}`)}
+      >
+        Read More
       </div>
     </div>
   );
--- src/pages/Pages.tsx
+++ src/pages/Pages.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Header } from "../components/Header";
 import { Navbar } from "../components/Navbar";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { Home } from "./Home";
 import { Doctors } from "./Doctors";
 import { AdminLogin } from "./AdminLogin";
@@ -9,6 +9,7 @@ import { Specialities } from "./Specialities";
 import { Event } from "./Event";
 import { AddEvent } from "./AddEvent";
 import { AddDoctor } from "./AddDoctors";
+import { EventDetails } from "../components/eventComponents/EventDetails";
 
 export const Pages = () => {
   return (
@@ -16,13 +17,18 @@ export const Pages = () => {
       <Header />
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Navigate replace to="/home" />}></Route>
         <Route path="/home" element={<Home />}></Route>
         <Route path="/doctors" element={<Doctors />}></Route>
         <Route path="/specialities" element={<Specialities />}></Route>
         <Route path="/admin-login" element={<AdminLogin />}></Route>
+
+        {/* Event Routes */}
         <Route path="/event" element={<Event />}></Route>
         <Route path="/event/add" element={<AddEvent />}></Route>
+        <Route path="/event/:id/:title" element={<EventDetails/>}></Route>
+
+        {/* Doctor Routes */}
         <Route path="/doctor/add" element={<AddDoctor />}></Route>
         {/* <Route path='/model' element={<AddSuccess/>}></Route> */}
       </Routes>
